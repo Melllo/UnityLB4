19b00e03ca4644e621170e5ce880b8ff1ef5485f
Irene Herbei <trisha19991410@gmail.com>
2021-03-07 23:19:18 +0200

    lb 4

---
+++ Assets/Scripts/LR_Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LR_Test : MonoBehaviour
+{
+    [SerializeField] private ClueLineRenderer line;
+    [SerializeField] private Transform[] points;
+
+
+    private void Start() {
+        line.SetUpLine(points);
+    }
+}
